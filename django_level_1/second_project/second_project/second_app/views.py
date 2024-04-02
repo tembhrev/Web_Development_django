212fccabfe94789650e098e1d45fa53edc8c1542
tembhrev <sw.vikastembhre@gmail.com>
2024-04-02 17:19:49 +0530

    add template dir for second_app

--- django_level_1/second_project/second_project/second_app/views.py
+++ django_level_1/second_project/second_project/second_app/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse("<em>My Second App</em>")
+def help(request):
+    my_dict = {'help_me': "I need help, I am in trouble!"}
+    return render(request, 'second_app/help.html', context=my_dict)
