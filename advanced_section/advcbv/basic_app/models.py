842f100ac7b923a3c7e1de9d9341f07aff528028
tembhrev <sw.vikastembhre@gmail.com>
2024-04-05 17:27:34 +0530

    Add CRUD Views

--- advanced_section/advcbv/basic_app/models.py
+++ advanced_section/advcbv/basic_app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
 class School(models.Model):
@@ -9,6 +11,9 @@ class School(models.Model):
     def __str__(self):
         return self.name
     
+    def get_absolute_url(self):
+        return reverse("basic_app:detail", kwargs={'pk':self.pk})
+    
 class Student(models.Model):
     name = models.CharField(max_length=256)
     age = models.PositiveBigIntegerField()
@@ -16,3 +21,4 @@ class Student(models.Model):
     
     def __str__(self):
         return self.name
+    
