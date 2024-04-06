5571d29bd1b690d81beeb3dd63367f5828d46072
tembhrev <sw.vikastembhre@gmail.com>
2024-04-06 16:05:02 +0530

    Add login.html

--- blog_project/mysite/mysite/urls.py
+++ blog_project/mysite/mysite/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views
+# from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    path('', include('blog.urls')),
+    path('accounts/login/', views.LoginView, name='login'),
+    path('accounts/logout/', views.LogoutView, name='logout', kwargs={'next_page':'/'}),
 ]
